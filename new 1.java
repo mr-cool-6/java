30cf34740eb71d4c45555366f31edb6fb0fe183f
mr-cool-6 <62960243+mr-cool-6@users.noreply.github.com>
2020-10-11 12:25:01 +0530

    Add files via upload

---
+++ new 1.java
@@ -0,0 +1,18 @@
+public class Myclass{
+    public static void main(String[] args) {
+        String name = "NAGAVIJAY";
+	    long DOB = 19_06_1998;
+	    String M_O_B = "June";
+	    int Birth_year= 1998;
+	    double Monthly_salary = 41666.6667;
+	    long salary_per_annam_in_lakhs= 500000;
+	     int tax_amount_in_percentage = 28;
+	    System.out.println(name);
+	    System.out.println(DOB);
+	    System.out.println(M_O_B);
+	    System.out.println(Birth_year);
+	    System.out.println(Monthly_salary);
+	    System.out.println(salary_per_annam_in_lakhs);
+	    System.out.println(tax_amount_in_percentage);
+	}
+}
